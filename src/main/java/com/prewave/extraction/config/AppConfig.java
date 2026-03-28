0d700f5bb457a41e324f7f69c05957b9285ddf1c
zytfo <khayalievartur@gmail.com>
2026-03-28 11:47:32 +0100

    add a redirect to docs

--- src/main/java/com/prewave/extraction/config/AppConfig.java
+++ src/main/java/com/prewave/extraction/config/AppConfig.java
@@ -4,14 +4,21 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableConfigurationProperties(PrewaveApiProperties.class)
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
     @Bean
     public RestClient restClient(PrewaveApiProperties properties) {
         return RestClient.builder()
                 .baseUrl(properties.baseUrl())
                 .build();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/docs");
     }
 }
